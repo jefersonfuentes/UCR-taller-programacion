3cc17ef9b0078ed4279c33c65960d2a96967c6ff
Jeferson <jeferson.fg710@gmail.com>
2023-02-14 10:17:35 -0600

    first commit

---
+++ introduccion.py
@@ -0,0 +1,36 @@
+# Esto es un comentario simple en Python
+
+# Mostrar salida en pantalla del mensaje Hola mundo.  
+print("Hola mundo")
+
+
+''' Comentario múltiple en Python
+Segunda línea del comentario
+Tercer línea del comentario.
+'''
+
+# Variables y tipos de datos en Python
+
+# Ejemplo de variable entera
+
+variable_uno = 25
+print("El valor de la variable entera 1 es: " , variable_uno)
+
+# Ejemplo de variable decimal
+
+variable_dos = 130.47
+print("El valor de la variable decimal dos es: " , variable_dos)
+
+# Ejemplo de variable boolena
+
+estadoActivo = False
+print("Estado", not estadoActivo)
+
+# Ejemplo de String o "Cadena de texto"
+
+nombre = "Jeferson Fuentes"
+print(f"Mi nombre es {nombre}")
+
+# Ejemplo de CONSTANTE
+PI = 3.1416 # Se declara en mayúscula para identificar que es una CONSTANTE.
+
